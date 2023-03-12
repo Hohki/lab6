d17c5a7e5f5059a5ab088a714458fccdeb04c95a
Khaled <75619589+Hohki@users.noreply.github.com>
2023-03-12 04:48:38 +0100

    Update Plockhändelse.java

--- event/Plockhändelse.java
+++ event/Plockhändelse.java
@@ -1,9 +1,18 @@
 package lab6.event;
 import lab6.state.Kassakö;
+import lab6.state.State;
+import lab6.tools.Pair;
 
 public class Plockhändelse extends Event {
 	Kassakö kassa = new Kassakö();
 	kassa.add();
 
+	 public Plockhändelse(Pair pair) {
+		 super("Plock", pair);
+	 }
 
+	@Override
+	public void effect(State state) {
+
+	}
 }
