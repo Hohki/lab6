6d2a6e22901bcb370520c830ad3cec6916274211
Trinaxez <127046963+Trinaxez@users.noreply.github.com>
2023-03-13 14:18:11 +0100

    wa

--- state/Kunder.java
+++ state/Kunder.java
@@ -8,10 +8,19 @@ package lab6.state;
 
 public class Kunder {
 	private int customerNumber;
-	
+
+	/**
+	 * @param number
+	 * @return nothing
+	 * */
 	public Kunder(int number) {
 		this.customerNumber = number;
 	}
+
+	/**
+	 * @param Nothing
+	 * @return customerNumber
+	 * */
 
 	public int getID() {
 		return this.customerNumber;
--- state/UniformRandomStream.java
+++ state/UniformRandomStream.java
@@ -13,18 +13,32 @@ public class UniformRandomStream {
 
 	private Random rand;
 	private double lower, width;
-	
+
+	/**
+	 * @param lower, upper seed
+	 * @return nothing
+	 * */
 	public UniformRandomStream(double lower, double upper, long seed) {
 		rand = new Random(seed);
 		this.lower = lower;
 		this.width = upper-lower;
 	}
-	
+
+	/**
+	 * @param lower, upper
+	 * @return nothing
+	 * */
+
 	public UniformRandomStream(double lower, double upper) {
 		rand = new Random();
 	    this.lower = lower;
 	    this.width = upper-lower;
 	}
+
+	/**
+	 * @param nothing
+	 * @return lower+rand.nextDouble()*width
+	 * */
 	
 	public double next() {
 	    return lower+rand.nextDouble()*width;
